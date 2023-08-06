760fa3fda827131b4e5301da438a0b2d12f3ba69
diego <guatemaladiegojose@gmail.com>
2023-08-06 09:56:03 -0400

    completed delete and update actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :plants, only: [:index, :show, :create]
+
+  # resources :plants, only: [:index, :show, :create, :update, :destroy]
+  #becauce we now have all five RESTFUL routes, we can omit the only above.
+  resources :plants
 end
